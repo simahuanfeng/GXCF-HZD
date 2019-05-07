aa82391046e4ef610d4d56b48c3fb4a22608a155
simahuanfeng <398869297@qq.com>
2019-05-07 15:19:09 +0800

    初始化项目

---
+++ GXCF-HZD/GXCF-HZD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GXCF-HZD
+//
+//  Created by 张振宇 on 2019/5/7.
+//  Copyright © 2019 张振宇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GXCF-HZD/GXCF-HZDTests/GXCF_HZDTests.swift
@@ -0,0 +1,34 @@
+//
+//  GXCF_HZDTests.swift
+//  GXCF-HZDTests
+//
+//  Created by 张振宇 on 2019/5/7.
+//  Copyright © 2019 张振宇. All rights reserved.
+//
+
+import XCTest
+@testable import GXCF_HZD
+
+class GXCF_HZDTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
